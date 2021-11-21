e1133b489fefbf353cc8371931f9d641808cf9e9
KevinBonnoron <2421321+KevinBonnoron@users.noreply.github.com>
2021-11-21 17:52:21 +0000

    fix: packages

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='the_keyspy',
-    packages=['the_keyspy'],
-    version='0.0.7',
+    packages=find_packages(include=('the_keyspy*',)),
+    version='0.0.8',
     description='The Keys Api',
     author='Kevin Bonnoron',
     author_email='kevin.bonnoron@gmail.com',
     url='https://github.com/KevinBonnoron',
-    download_url='https://github.com/KevinBonnoron/the_keyspy/archive/refs/tags/v0.0.7.tar.gz',
+    download_url='https://github.com/KevinBonnoron/the_keyspy/archive/refs/tags/v0.0.8.tar.gz',
     license='MIT',
     install_requires=['dataclasses_json', 'requests'],
     setup_requires=['pytest-runner'],
